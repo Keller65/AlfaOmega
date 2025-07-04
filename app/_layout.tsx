6c9f21c81e232fe80166d23d68dc1dd5f4171d04
Aerley <tecpointdistribucion2@gmail.com>
2025-07-04 13:03:39 -0600

    quitando el header del login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout() {
             }}
           >
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="login" options={{ headerShown: false }} />
           </Stack>
         </BottomSheetModalProvider>
       </AuthProvider>
