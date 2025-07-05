fb08d6c3abeaac84f9ac0041242f4955a91816ea
Aerley <tecpointdistribucion2@gmail.com>
2025-07-04 18:48:01 -0600

    order icon actualizado

--- assets/icons/OrdeIcon.jsx
+++ assets/icons/OrdeIcon.jsx
@@ -4,12 +4,13 @@ const SvgComponent = (props) => (
   <Svg
     {...props}
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 512 512"
     width={props.size || 24}
     height={props.size || 24}
     fill={props.color || "currentColor"}
+    className="icon icon-tabler icons-tabler-filled icon-tabler-layout-board"
   >
-    <Path d="M509.5 184.6 458.9 32.8C452.4 13.2 434.1 0 413.4 0H272v192h238.7c-.4-2.5-.4-5-1.2-7.4zM240 0H98.6c-20.7 0-39 13.2-45.5 32.8L2.5 184.6c-.8 2.4-.8 4.9-1.2 7.4H240V0zM0 224v240c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V224H0z" />
+    <Path fill="none" d="M0 0h24v24H0z" />
+    <Path d="M5 3h5a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V5a2 2 0 0 1 2-2M14 3h5a2 2 0 0 1 2 2v8a1 1 0 0 1-1 1h-6a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1M14 16h6a1 1 0 0 1 1 1v2a2 2 0 0 1-2 2h-5a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1M4 10h6a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1H5a2 2 0 0 1-2-2v-8a1 1 0 0 1 1-1" />
   </Svg>
 )
 export default SvgComponent
