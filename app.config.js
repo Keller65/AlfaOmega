36fd74c91b156b3ff5693e75da75f2609fd52f25
Aerley <tecpointdistribucion2@gmail.com>
2025-07-28 16:08:43 -0600

    usando expo version ~52.0.47

--- app.config.js
+++ app.config.js
@@ -1,9 +1,11 @@
+import { withPlugins } from '@expo/config-plugins';
+
 const config = {
   expo: {
     name: 'Alfa & Omega',
     slug: 'alfa-and-omega',
     version: '1.0.0',
-    sdkVersion: '53.0.0',
+    sdkVersion: '52.0.20',
     userInterfaceStyle: 'automatic',
     android: {
       package: 'com.aerley_adkins.alfaandomega',
@@ -15,17 +17,17 @@ const config = {
           android: {
             compileSdkVersion: 34,
             targetSdkVersion: 34,
-            kotlinVersion: '1.8.0',
           },
+          kotlinVersion: '1.8.0',
         },
       ],
     ],
     extra: {
       eas: {
-        projectId: "2b76a3bb-99fb-4601-8265-be0f86eaec71"
-      }
-    }
-  }
+        projectId: '2b76a3bb-99fb-4601-8265-be0f86eaec71',
+      },
+    },
+  },
 };
 
 export default config;
